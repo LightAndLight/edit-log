bf759f1b6d8c830c5e219a0a6f142495d0d89bef
Isaac Elliott <isaace71295@gmail.com>
2020-10-10 16:44:14 +1000

    fix reflex-platform submodule

--- reflex-platform/nixpkgs-overlays/all-cabal-hashes/default.nix
+++
@@ -1,8 +0,0 @@
-self: _: {
-
-  all-cabal-hashes = self.fetchurl {
-    url = https://github.com/commercialhaskell/all-cabal-hashes/archive/a47bf76d8dabd429cd50f44761a6bea8f552a93a.tar.gz;
-    sha256 = "126xq53kj9sxnf7si6vhr91xx7zr67371ihrpx3cm93ayyv8n25s";
-  };
-
-}
